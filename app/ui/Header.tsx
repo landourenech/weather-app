35fca041b3734e6fbeebef22c115fa7a29ebb428
landou <landoumechack@gmail.com>
2024-11-25 14:38:31 +0100

    update_weather_version2

--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -1,7 +1,7 @@
 export default function WeatherHeader() {
   return (
     <div className="text-center mb-8">
-      <h1 className="text-4xl font-bold mb-2">Riga, Latvia</h1>
+      <h1 className="text-4xl font-bold mb-2">Libreville, Gabon</h1>
       <p className="text-xl opacity-90">
         {new Date().toLocaleDateString('fr-FR', {
           weekday: 'long',
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-  "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
-  // Or if using `src` directory:
-  "./src/**/*.{js,ts,jsx,tsx,mdx}",
-];
-export const theme = {
-  extend: {},
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Adaptez ce chemin à la structure de votre projet
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
-export const plugins = [];
